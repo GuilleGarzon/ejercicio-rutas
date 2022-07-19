7c4837e2d0f4eb35abe20167eb25c23b162c1509
GuilleGarzon <guillegarzon.gg@gmail.com>
2022-07-18 19:41:04 -0500

    feat: add exercises routes

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+function Home () {
+  return (
+    <>
+      <h1>Home</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+      Facere commodi, repudiandae neque eos rem accusamus, repellat corrupti harum 
+      nam aperiam sit ducimus doloribus quam. Consequatur similique consequuntur eos unde cumque.</p>
+    </>
+  )
+}
+
+export default Home;
---
+++ src/components/Pricing.jsx
@@ -0,0 +1,12 @@
+function Pricing () {
+  return (
+    <>
+      <h1>Pricing</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+      Facere commodi, repudiandae neque eos rem accusamus, repellat corrupti harum 
+      nam aperiam sit ducimus doloribus quam. Consequatur similique consequuntur eos unde cumque.</p>
+    </>
+  )
+}
+
+export default Pricing;
